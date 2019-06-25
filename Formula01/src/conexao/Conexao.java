1c1475141d6027e261a63e5e30d14aee69e7d4cc
Administrador <Administrador@10.10.4.23>
2019-06-25 17:47:44 -0300

    lsdg

--- Formula01/src/conexao/Conexao.java
+++ Formula01/src/conexao/Conexao.java
@@ -18,7 +18,7 @@ public class Conexao {
     private static Connection conn;
     private final static String driver = "org.postgresql.Driver";
     private final static String ip = "localhost/";
-    public static String dataBase = "aula";
+    public static String dataBase = "Formula01";
     public static String user = "postgres";
     public static String password = "postgres";
 
@@ -53,8 +53,8 @@ public class Conexao {
             System.out.println("Feito!");
         } catch (SQLException ex) {
             Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
+        } catch (ClassNotFoundException ex){
             Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
+    } 
 }
